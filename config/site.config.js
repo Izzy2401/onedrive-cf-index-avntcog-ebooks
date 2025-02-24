88fb567066b961a09c7396638d7abcabe113f2f2
Izzy2401 <136386685+Izzy2401@users.noreply.github.com>
2025-02-23 22:52:08 -0400

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -35,7 +35,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'For those who enjoy reading or desire to read more. Test',
+    'Please download files and open with one of the following: <a href="https://play.google.com/store/apps/details?id=com.flyersoft.moonreader&hl=en" target="_blank" rel="noopener noreferrer">Download Moon+ Reader here</a>. For iOS, open with Apple Books.',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
